b21a8484444936078a1c5e1d8f45656a435913f3
Benjamin Smith <benjamins@unity3d.com>
2018-10-17 15:25:03 -0700

    Adds lwrp packages and updates Effects test to lwrp.

---
+++ ScriptableRenderPipeline/com.unity.render-pipelines.lightweight/LWRP/Data/LightweightPipelineResources.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LightweightPipelineResources : ScriptableObject
+{
+    public Shader BlitShader;
+    public Shader CopyDepthShader;
+    public Shader ScreenSpaceShadowShader;
+    public Shader SamplingShader;
+}
